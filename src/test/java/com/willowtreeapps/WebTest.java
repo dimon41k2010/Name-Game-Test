79ca5af6f6548c35802fc564ebdd838850363501
dimon41k2010 <37424372+dimon41k2010@users.noreply.github.com>
2018-03-19 07:49:13 -0700

    Update WebTest.java

--- src/test/java/com/willowtreeapps/WebTest.java
+++ src/test/java/com/willowtreeapps/WebTest.java
@@ -22,7 +22,14 @@ public class WebTest {
         System.setProperty("webdriver.chrome.driver", "chromedriver.mac");
         Capabilities capabilities = DesiredCapabilities.chrome();
         driver = new ChromeDriver(capabilities);
-        driver.navigate().to("http://www.ericrochester.com/name-game/");
+        //driver.manage().window().maximize();
+
+    }
+
+    @After
+    public void teardown() {
+        driver.quit();
+        System.clearProperty("webdriver.chrome.driver");
     }
 
     @Test
@@ -37,10 +44,35 @@ public class WebTest {
                 .validateClickingFirstPhotoIncreasesTriesCounter();
     }
 
-    @After
-    public void teardown() {
-        driver.quit();
-        System.clearProperty("webdriver.chrome.driver");
+    //_________________________________________________________________________________________//
+
+    //Test #1 Verify the "streak" counter is incrementing on correct selections;
+    @Test
+    public void test_clicking_correct_photo_increases_streak_counter() {
+        new HomePage(driver)
+                .validateClickingCorrectPhotoIncreasesStreakCounter();
+    }
+
+    //Test #2 Verify the a multiple вЂњstreakвЂќ counter resets after getting an incorrect answer;
+    @Test
+    public void test_multiple_streak_counter_resets_after_incorrect_answer() {
+        new HomePage(driver)
+                .validateClickingWrongPhotoResetsMultipleStreakCounter();
+    }
+
+    //Test #3 Verify that after 10 random selections the correct counters are being incremented for tries and correct counters;
+    @Test
+    public void test_10_random_selections_increment_counters () {
+        new HomePage(driver)
+                .validateRandomSelectionsIncrementCounters();
+    }
+
+
+    //Test #4 Verify name and displayed photos change after selecting the correct answer;
+    @Test
+    public void test_name_and_photos_change_after_correct_selection() {
+        new HomePage(driver)
+                .validateClickingCorrectPhotoChangesNameAndPhotos();
     }
 
 }
